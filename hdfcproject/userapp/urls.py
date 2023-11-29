6008f691097157787bcb932b0d6d45f4797bde93
SRINIVASAREDDY TAMMA <tammasrinivasareddz@gmail.com>
2023-11-30 01:01:02 +0530

    Ignore hdfcproject

--- hdfcproject/userapp/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import index, about, contact
-
-urlpatterns = [
-    path('index/', index, name='index'),
-    path('about/', about, name='about'),
-    path('contact/', contact, name='contact'),
-]
